50a7e503c4c78630519fb1c4a62b13f7020e2799
Runnerman8872 <STE22002958@btc.ac.uk>
2024-08-08 11:58:36 +0100

    first commit

---
+++ Landing page.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+        <title>Isaacs Book</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel = "stylesheet" href = "Shop.css">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <p>Test</p>
+        <nav class="navbar navbar-expand-lg" style="background-color: #A84E4F;">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="HomePage.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="HomePage.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ShopPage.php">Shop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ShipPage.php">Basket</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            User
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="LoginPage.php">Login</a></li>
+            <li><a class="dropdown-item" href="SignoutPage.php">Log out</a></li>
+            <li><a class="dropdown-item" href="SignupPage.php">Sign up</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+    </body>
+</html>
