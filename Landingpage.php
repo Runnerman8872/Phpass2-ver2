d1bd838751e7e86a2a98bb403fdc8c9dca350d80
Runnerman8872 <STE22002958@btc.ac.uk>
2024-08-08 14:09:17 +0100

    Added Formatting Stuff

--- Landingpage.php
+++ Landingpage.php
@@ -1,13 +1,13 @@
 <html>
     <head>
     <meta charset="utf-8">
-        <title>Isaacs Book</title>
+        <title>Isaacs Books</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel = "stylesheet" href = "Shop.css">
     </head>
     <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <p>Test</p>
+
         <nav class="navbar navbar-expand-lg" style="background-color: #A84E4F;">
     <div class="container-fluid">
     <a class="navbar-brand" href="HomePage.php">Navbar</a>
@@ -17,7 +17,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="HomePage.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="Landingpage.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="ShopPage.php">Shop</a>
@@ -37,6 +37,10 @@
         </li>
       </ul>
     </div>
+  </div>
+  </nav>
+  <div class="flex-container">
+    <p>this text is a test for formattings sake</p>
   </div>
     </body>
 </html>
