4faf0032181fde5a3836657ddb3cfcb33552b074
Runnerman8872 <STE22002958@btc.ac.uk>
2024-08-14 15:19:18 +0100

    added basuc login functionality

--- Shoppage.php
+++ Shoppage.php
@@ -1,1 +1,82 @@
-<p>Shoppage</p>
+<?php
+  $logged_in = false;
+
+?>
+
+<html>
+    <head>
+    <meta charset="utf-8">
+        <title>Isaacs Books</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel = "stylesheet" href = "Shop.css">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+        <nav class="navbar navbar-expand-lg" style="background-color: #A84E4F;" data-bs-theme="dark"
+    <div class="container-fluid">
+    <a class="navbar-brand" href="HomePage.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="Landingpage.php"><ntc>Home</ntc></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="Shoppage.php"><ntc>Shop</ntc></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Shippage.php"><ntc>Basket</ntc></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <ntc>User</ntc>
+          </a>
+          <ul class="dropdown-menu">
+            <?php
+            
+            if(isset($_POST["Logout"])){
+              $logged_in = true;
+            }
+
+            if(isset($_POST["Login"])){
+              $logged_in = false;
+            }
+
+            if ($logged_in == true){
+              echo'<form method="post"> 
+                <input type="submit" name="Login"
+                class="button" value="Login" />
+                </form> ';
+            }
+            else{
+              echo '<form method="post"> 
+                <input type="submit" name="Logout"
+                class="button" value="Logout" />
+                </form> ';
+            }
+            ?>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+  <div class="flex-container">
+    <p>this text is a test for formattings sake</p>
+    <?php
+    if ($logged_in == true){
+      echo "true";
+    }
+    else{
+      echo "false";
+    }
+    ?>
+  </div>
+    </body>
+  <footer>
+    <p>this text is a test for formattings sake</p>
+  </footer>
+</html>
