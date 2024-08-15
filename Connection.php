86ece29e9eaed055b0afd21f01038935716d1083
Runnerman8872 <STE22002958@btc.ac.uk>
2024-08-15 14:10:12 +0100

    Code re-formatted

---
+++ Connection.php
@@ -0,0 +1,42 @@
+<p>H test</p>
+<?php
+$type = "mysql";
+$server = "localhost"; #V
+$db = "isaacsbooks"; #V
+$port = "3306";
+$charset = "utf8mb4";
+
+$username = "root"; #V
+$password = ""; #V
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
+try{
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), $e->getCode());
+}
+$name = "Tim";
+
+$stmt = $pdo -> query("SELECT UserName FROM isaacsbooksuser");
+#$stmt -> execute([$name]);
+#$user = $stmt->fetch();
+
+foreach ($stmt as $row)
+{
+    echo $row["UserName"]."\n";
+}
+$logged_in = false;
+if(isset($_POST["Logout"])){
+  $logged_in = true;
+}
+
+if(isset($_POST["Login"])){
+  $logged_in = false;
+}
+  echo"php test";
+?>
